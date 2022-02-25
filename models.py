181d8e88d08fed34577a8be45b88a95b384bbfd5
leandrovargasEltirri <93355834+leandrovargasEltirri@users.noreply.github.com>
2022-02-24 21:05:27 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,26 +1,26 @@
 from django.db import models
-from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=60)
     apellido = models.CharField(max_length=100)
-    DNI = models.CharField(unique=True)
-    nacimiento = models.CharField(max_length=50,blank=True)
-    edad = models.CharField(max_length=50)
-    Correo = models.EmailFierld(unique=True)
-    club= models.CharField(max_length=50)
+    dni=models.CharField(max_length=8)
+    fecha=models.DateField(auto_now=True)
+    club = models.CharField(max_length=50)
     foto = models.ImageField(
         upload_to = 'foto/%Y/%m/%d',
         blank = True,
-        verbose_name = ('Foto del docente')
+        verbose_name = ('Foto de la persona')
     )
+  
+    
 
 class Meta:
-    verbose_name = ('docente')
-    verbose_name_plural = ('docentes')
+    verbose_name = ('Persona')
+    verbose_name_plural = ('Persona')
+    
+    
 
 def __str__(self):
-    return self.nombre
+    return self.
